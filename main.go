76fc3cf435bb7e9089802d2a584add0ba1af9486
alugo <abelardo.lugo@mercadolibre.com>
2022-09-12 16:10:48 -0300

    main.go, store and tests

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hey Go URL Shortener",
+		})
+	})
+
+	err := r.Run(":9808")
+	if err != nil {
+		panic(fmt.Sprintf("Failed to start the web server - Error: %v", err))
+	}
+
+}
